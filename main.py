4d8b3f357f7acc17be335b5765131dba771a9ba6
marcflight <markus.lovenjak@web.de>
2025-03-16 22:41:15 +0100

    Add Cell Logic

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from graphics import Window, Line, Point
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(l, "black")
+    
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(50, 50, 100, 100)
+    
+    c = Cell(win)
+    c.has_right_wall = False
+    c.has_top_wall = False
+    c.draw(100, 100, 150, 150)
+    
+    c = Cell(win)
+    c.draw(200, 200, 250, 250)
+    #l = Line(Point(50, 50), Point(400, 400))
+    #win.draw_line(l, "black")
     win.wait_for_close()
     
 
